23342ac2068c7519ee266f851fb655e003cb9d9e
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-12-20 16:37:40 +0100

    feat: initialize project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello world",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+h1 {
+  color: red;
+}
+</style>
